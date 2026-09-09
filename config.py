fd0d0fdb491180f5feafb61e7bb6eeff6abc3848
ee956658-source <ee956658@gmail.com>
2026-09-09 23:07:41 +0530

    Update bot configuration values in config.py

--- config.py
+++ config.py
@@ -16,10 +16,10 @@ MONGO_DB_URL    = os.environ["MONGO_DB_URL"]
 OWNER_ID        = int(os.environ["OWNER_ID"])
 
 # ── Optional ──────────────────────────────────────────────────────────────────
-BOT_NAME         = os.getenv("BOT_NAME", "Shizu Music")
-BOT_LINK         = os.getenv("BOT_LINK", "https://t.me/ShizuMusicBot")
-UPDATES_CHANNEL  = os.getenv("UPDATES_CHANNEL", "https://t.me/PBX_UPDATE")
-SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "https://t.me/PBXCHATS")
+BOT_NAME         = os.getenv("BOT_NAME", "YORXMUSIC")
+BOT_LINK         = os.getenv("BOT_LINK", "https://t.me/YorMusicXbot")
+UPDATES_CHANNEL  = os.getenv("UPDATES_CHANNEL", "https://t.me/VIP_PFP_SP")
+SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "https://t.me/zpaveldurov")
 LOGGER_ID        = int(os.getenv("LOGGER_ID", "0"))
 PING_IMG_URL     = os.getenv("PING_IMG_URL", "https://files.catbox.moe/ddzvc0.jpg",)
 SESSION_NAME     = os.getenv("SESSION_NAME", "ShizuMusic")
